5f9826bd1b79b57bba9f71dfbb24aec2dd14323b
grifonna8 <grifonna8@gmail.com>
2020-01-23 16:08:18 +0300

    main work of 1st lesson

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let money = 3;
+let income = 20;
+let addExpenses = "добавить расходы";
+let deposit = 300;
+let mission = "миссия";
+let period = 16;
+
+alert("Она пытается в JS");
+console.log("Все еще пытается");
