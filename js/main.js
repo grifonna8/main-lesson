2601772e58575f847dfec9de701653c6853102a5
grifonna8 <grifonna8@gmail.com>
2020-01-27 17:56:17 +0300

    main 3rd lesson

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
-let money = 10000,
-income = 20,
-addExpenses = "добавить расходы",
-deposit = 300,
-mission = "миссия",
-period = 16;
+let money = prompt('Ваш месячный доход?'),
+income = '10000',
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+deposit = confirm('Есть ли у вас депозит в банке?'),
+mission = 1000000,
+period = 8,
+budgetDay = null,
+budgetMonth = null,
+expenses1 = prompt('Введите обязательную статью расходов?'),
+expenses2 = prompt('Введите обязательную статью расходов?'),
+amount1 = prompt('Во сколько это обойдется?'),
+amount2 = prompt('Во сколько это обойдется?');
 
-alert("Она пытается в JS");
-console.log("Все еще пытается");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен', period, 'месяцев');
+console.log('Цель заработать', mission, 'рублей');
+console.log((addExpenses.toLowerCase()).split(', '));
+budgetMonth = money - amount1 - amount2;
+console.log('Бюджет на месяц', budgetMonth);
+period = Math.ceil(mission / budgetMonth); /* округление вверх */
+console.log('Цель будет достигнута за', period, 'месяцев'); 
+budgetDay = Math.floor(budgetMonth / 30);/* округление в меньшую сторону */
+console.log('Бюджет на день:', budgetDay);
+
+if (budgetDay >= 1200){
+  console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay >= 0 && budgetDay < 600) {
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+} else {
+  console.log('Что-то пошло не так');
+}
