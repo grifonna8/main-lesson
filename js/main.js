7705da9e145591c52e76101d5991e0b42a0c38e4
grifonna8 <grifonna8@gmail.com>
2020-02-03 17:32:54 +0300

    homework for lesson 6

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
-let money = 10000,
-income = 20,
-addExpenses = "добавить расходы",
-deposit = 300,
-mission = "миссия",
-period = 16;
+'use strict';
 
-alert("Она пытается в JS");
-console.log("Все еще пытается");
+let isNumber = function(n){
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+let answer = +prompt('Угадай число от 1 до 100');
+function bot(){
+  let a = 35;
+  if (isNumber(answer) && answer !== 0){
+    if (answer > a){
+      answer = +prompt('Загаданное число меньше');
+      return bot();
+    } else if (answer < a && answer !== 0){
+      answer = +prompt('Загаданное число больше');
+      return bot();
+    } else if (answer === a && answer !== 0){
+      answer = confirm('Вы угадали!');
+      if (answer === true) {
+        answer = +prompt('Угадай число от 1 до 100');
+        return bot();
+      } else {
+        return alert('Игра окончена');
+      }
+    } else if (answer === 0) {
+      return alert('Игра окончена');
+    }
+  } else {
+    alert('Введи число');
+    answer = +prompt('Угадай число от 1 до 100');
+    return bot(); 
+  }
+}
+
+bot();
+
