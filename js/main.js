7fcd51a39940302902b1227e62d6037ded5cd2d0
grifonna8 <grifonna8@gmail.com>
2020-02-27 16:08:45 +0300

    feat:final homework for lesson 14 part 1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
 'use strict';
-console.log('help');
+let items = document.querySelector('.items');
+const DomElement = function(){
+  this.selector =  prompt('Введите селектор');
+  this.height = '80px';
+  this.width = '150px';
+  this.bg =  'pink';
+  this.fontSize = '14px';
+};
+
+DomElement.prototype.creating = function(){
+  if (this.selector[0] === '.'){
+    let newElem = document.createElement('div');
+    newElem.className = this.selector.slice(1);
+    newElem.style.cssText = `height:${this.height};
+    width:${this.width};
+    background-color:${this.bg};
+    font-size:${this.fontSize};`;
+    newElem.innerText = 'Я новый элемент';
+    items.appendChild(newElem);
+  } else if (this.selector[0] === '#'){
+    let newElem = document.createElement('p');
+    newElem.id = this.selector.slice(1);
+    newElem.style.cssText = `height:${this.height};
+    width:${this.width};
+    background-color:${this.bg};
+    font-size:${this.fontSize};`;
+    newElem.innerText = 'Я новый элемент';
+    items.appendChild(newElem);
+  }
+};
+let elements = new DomElement();
+elements.creating();
