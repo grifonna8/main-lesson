07ee3e3cb5d90916aea09ef96c0cd29ddb50939c
grifonna8 <grifonna8@gmail.com>
2020-03-02 19:40:21 +0300

    clear working space

--- js/main.js
+++
@@ -1,9 +0,0 @@
-let money = 10000,
-income = 20,
-addExpenses = "добавить расходы",
-deposit = 300,
-mission = "миссия",
-period = 16;
-
-alert("Она пытается в JS");
-console.log("Все еще пытается");
